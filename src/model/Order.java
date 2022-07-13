9ac0208ca151088852fdeb012de897b3f9de4769
stav <stav134679@gmail.com>
2022-07-13 17:29:50 +0300

    Added shieeet

--- src/model/Order.java
+++ src/model/Order.java
@@ -21,4 +21,60 @@ public class Order {
         this.orderTimeStamp = orderTimeStamp;
         this.deliveryMan = deliveryMan;
     }
+
+    public float getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(float totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Item[] getItems() {
+        return items;
+    }
+
+    public void setItems(Item[] items) {
+        this.items = items;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getOrderAddress() {
+        return orderAddress;
+    }
+
+    public void setOrderAddress(String orderAddress) {
+        this.orderAddress = orderAddress;
+    }
+
+    public String getOrderTimeStamp() {
+        return orderTimeStamp;
+    }
+
+    public void setOrderTimeStamp(String orderTimeStamp) {
+        this.orderTimeStamp = orderTimeStamp;
+    }
+
+    public DeliveryMan getDeliveryMan() {
+        return deliveryMan;
+    }
+
+    public void setDeliveryMan(DeliveryMan deliveryMan) {
+        this.deliveryMan = deliveryMan;
+    }
 }
